7826f3374dd4c519a99ea8276617c74e1a55cd43
Priset <emanuel.1kll@gmail.com>
2024-09-20 21:38:45 -0400

    Implement Interface and logs and arrow function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface IPerson {
+  name:string
+  lastName: string
+  age?: number
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,9 +16,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title:number = 80;
+  animals:string[] = ['a','b','c','d','e','f','g']
+
+  person: IPerson = {
+    name: 'a',
+    lastName: 'b'
+  }
 
   constructor(){
     console.log('substract ', this.substract(8,4))
+
+    console.log('MAP:', this.animals.map( (animal:string) => ( animal + 'new')    ))
+    console.log('FOREACH:', this.animals.forEach( (animal) => ( animal + 'new')    ))
+    console.log('FIND', this.animals.find((animal)=>  animal === 'z'))
+    console.log('FILTER', this.animals.filter((animal)=>  animal === 'z'))
+    console.log('INDEXOF', this.animals.indexOf('c'))
   }
 
   public sum(num1: number, num2: number): number {
@@ -30,4 +48,21 @@ export class AppComponent {
       console.log('person = ', evenPersons[i]);
     }
   }
+
+  //  function sumar(){
+  //   return 1 + 2;
+  //  }
+
+  //  const suma = () => {
+  //   return 1 + 2
+  //  }
+
+
+  //  function resta(){
+  //   return 'hola' + a
+  //  }
+
+  // arrow functions
+  //  const resta = () => ('hola' + a)
+
 }
