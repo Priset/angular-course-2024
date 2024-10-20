8ceff1d9df0bcddaa7660a39712795b3bccb6a3f
Emanuel Lozano <emanuel.1kll@gmail.com>
2024-10-20 19:37:21 -0400

    [16-10-2024] Navigation per Module

---
+++ src/app/student/score/score.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-score',
+  templateUrl: './score.component.html',
+  styleUrl: './score.component.scss'
+})
+export class ScoreComponent {
+}
