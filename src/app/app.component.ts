10d8eee6b96fd7b8f099551b026514ae8ee3719f
Emanuel Lozano <emanuel.1kll@gmail.com>
2024-10-22 22:37:42 -0400

    [22-10-2024] reactive form, formControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import {PurePipe} from "./pure.pipe";
 import {ImpurePipe} from "./impure.pipe";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 interface IPerson {
   name: string;
@@ -35,12 +35,15 @@ interface IPerson {
     ImpurePipe,
     MatCardModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
+
+  scoreControl = new FormControl<string>('asdasdasd', [Validators.required])
 
   name:string = 'testName'
   lastName:string = ''
@@ -78,6 +81,11 @@ export class AppComponent {
     this.youtube.subscribe((res) => {
       console.log("SUSCRIBER 1: ", res);
     });
+
+    this.scoreControl.valueChanges.subscribe((res) => {
+      console.log('SCORE VALUE OBSERVABLE: ', res)
+    })
+
   }
   public sum(...persons: number[]) {
     //return persons[0] + persons[1]
@@ -173,6 +181,10 @@ export class AppComponent {
 
   onSubmit(data:any){
     console.log('TEMPLATE DRIVEN FORM: ', data)
+  }
+
+  onPrintScore(){
+    console.log('SCORE: ', this.scoreControl.value)
   }
 
 }
