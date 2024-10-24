7ae1a0ff4e25bd0f07f944dec1fa38dbf23838e1
Emanuel Lozano <emanuel.1kll@gmail.com>
2024-10-23 21:04:03 -0400

    [23-10-2024] reactive forms with untypedFormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,28 @@ import {PurePipe} from "./pure.pipe";
 import {ImpurePipe} from "./impure.pipe";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule, UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators
+} from "@angular/forms";
 
 interface IPerson {
   name: string;
   lastName: string;
   age?: number;
+}
+
+interface IForm {
+  name: string
+  score: string
+  school: string
+  proffesor: string
+  university: string
 }
 
 @Component({
@@ -75,10 +91,12 @@ export class AppComponent {
   youtube = from([1, 2, 3, 4, 5, 6]);
 
   studentForm!: FormGroup
+  student2Form!: UntypedFormGroup
 
   constructor(
     private router: Router,
-    private formBuilder: FormBuilder) {
+    private formBuilder: FormBuilder,
+    private untypedFormBuilder: UntypedFormBuilder) {
     const { name, age } = this.person;
     let both = [...this.students, ...this.parents];
 
@@ -97,6 +115,23 @@ export class AppComponent {
       proffesor: [''],
       university: ['']
     })
+
+    this.student2Form = this.untypedFormBuilder.group({
+      name: ['', Validators.required],
+      score: [''],
+      school: [''],
+      proffesor: [''],
+      university: ['']
+    })
+
+    /* this.studentForm = new FormGroup({
+      name: new FormControl<string>('sdasdasdasd', [Validators.required]),
+      score: new FormControl<string>('sdfsdfsdf'),
+      school: new FormControl<string>(''),
+      proffesor: new FormControl<string>(''),
+      university: new FormControl<string>('')
+    }) */
+
     this.studentForm.valueChanges.subscribe((res) => {
       console.log('FORM GROUP OBSERVABLE: ', res)
     })
