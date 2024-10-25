4a935bc94003d4edd509ba57bf39ff4172e3800b
Emanuel Lozano <emanuel.1kll@gmail.com>
2024-10-24 22:08:32 -0400

    [24-10-2024] global service

--- src/app/student/average/average.component.ts
+++ src/app/student/average/average.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../auth.service";
 @Component({
   selector: 'app-average',
   templateUrl: './average.component.html',
   styleUrl: './average.component.scss'
 })
 export class AverageComponent {
+
+  constructor(private _authService: AuthService) { }
+  onPrint(){
+    console.log('LOGGED USER: ', this._authService.getUser())
+  }
 }
